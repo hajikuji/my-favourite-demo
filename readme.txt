2326c4d3c0cff3aaf01183e1494bd21966e76f32
hajikuji <819771449@qq.com>
2019-06-06 11:50:02 +0800

    补充了Jquery的特效以及clip-path的使用

--- readme.txt
+++ readme.txt
@@ -3,3 +3,5 @@
 3.css3中的box-shadow方法：box-shadow: h-shadow v-shadow blur spread color inset;
 4.point-event:none   =>取消target
 5.content: attr(name) 返回name属性的字符串
+6.3D翻面的核心 设置backface-visibility: hidden; front back设置rotate
+7.clip-path(上,右,下,左);其中x,y指初始位置
---
+++ 火箭发射滚动到顶部/index.js
@@ -0,0 +1,20 @@
+$(function(){
+    $(window).scroll(function(){
+        if($(window).scrollTop()<100) {
+            $(".rocket").slideUp(500);
+        } else {
+            $(".rocket").slideDown(500);
+        }
+        })
+        $(".rocket").on("click",function(){
+            $(this).animate({
+                bottom: '100%'
+            },1000);
+            $("html,body").animate({
+                scrollTop: '0'
+            },1000);
+            setTimeout(function(){
+                $(".rocket").removeAttr("style");
+            },1200);
+        })
+})
